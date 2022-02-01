4cf98f4bb2f8f3fdb007c678bffed97ed45bae34
MaryGoo <m381988@gmail.com>
2022-02-01 06:23:56 +0300

    Task 3: Allure report 3

--- src/test/java/com/RegistrationTest.java
+++ src/test/java/com/RegistrationTest.java
@@ -31,7 +31,7 @@ public class RegistrationTest {
 
     @DisplayName("Не успешная регистрация с пустой формой")
     @Test
-    public void registrationEmptyFormFail() {
+    public void registrationEmptyFormFailTest() {
         userFake = new User();
         mainPage.clickSingIn();
         loginPage.clickRegister();
@@ -44,7 +44,7 @@ public class RegistrationTest {
 
     @DisplayName("Не успешная регистрация с пустым Email")
     @Test
-    public void registrationEmptyEmailFail() {
+    public void registrationEmptyEmailFailTest() {
         userFake = new User().getRandom(6);
         userFake.setEmail("");
         mainPage.clickSingIn();
@@ -58,7 +58,7 @@ public class RegistrationTest {
 
     @DisplayName("Не успешная регистрация с пустым Name")
     @Test
-    public void registrationEmptyNameFail() {
+    public void registrationEmptyNameFailTest() {
         userFake = new User().getRandom(6);
         userFake.setName("");
         mainPage.clickSingIn();
@@ -72,7 +72,7 @@ public class RegistrationTest {
 
     @DisplayName("Не успешная регистрация с пустым Name")
     @Test
-    public void registrationEmptyPasswordFail() {
+    public void registrationEmptyPasswordFailTest() {
         userFake = new User().getRandom(6);
         userFake.setPassword("");
         mainPage.clickSingIn();
@@ -86,7 +86,7 @@ public class RegistrationTest {
 
     @DisplayName("Успешная регистрация с паролем 6 символов")
     @Test
-    public void registrationWithPass6CharSuccess() {
+    public void registrationWithPass6CharSuccessTest() {
         userFake = new User().getRandom(6);
         mainPage.clickSingIn();
         loginPage.clickRegister();
@@ -98,7 +98,7 @@ public class RegistrationTest {
 
     @DisplayName("Не успешная регистрация с паролем 5 символов")
     @Test
-    public void registrationWithPass5CharFail() {
+    public void registrationWithPass5CharFailTest() {
         userFake = new User().getRandom(5);
         mainPage.clickSingIn();
         loginPage.clickRegister();
